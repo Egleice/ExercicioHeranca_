ddd7ec463f806186dcd5f9a1410dc23cef7e1090
Egleice <egleice.kelly20@gmail.com>
2026-04-22 16:48:49 -0300

    Create abstract Conta class for account management

---
+++ Conta.java
@@ -0,0 +1,19 @@
+abstract class Conta {
+    protected double saldo;
+    protected Cliente cliente;
+
+    public Conta(Cliente cliente, double saldo) {
+        this.cliente = cliente;
+        this.saldo = saldo;
+    }
+
+    public abstract boolean sacar(double valor);
+
+    public void depositar(double valor) {
+        saldo += valor;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+}
