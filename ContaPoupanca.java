9520a675c0d86304db21c208397a5caf5ee71940
Egleice <egleice.kelly20@gmail.com>
2026-04-22 16:49:57 -0300

    Add ContaPoupanca class with withdrawal and interest methods

---
+++ ContaPoupanca.java
@@ -0,0 +1,19 @@
+class ContaPoupanca extends Conta {
+
+    public ContaPoupanca(PessoaFisica cliente, double saldo) {
+        super(cliente, saldo);
+    }
+
+    @Override
+    public boolean sacar(double valor) {
+        if (saldo >= valor) {
+            saldo -= valor;
+            return true;
+        }
+        return false;
+    }
+
+    public void rendimento() {
+        saldo += saldo * 0.005;
+    }
+}
